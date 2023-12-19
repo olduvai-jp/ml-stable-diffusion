91b586d1e013bf044e8f4276d2985cfbc0e4df9d
dyamagishi <daiki.yamagishi@olduvai.jp>
2023-12-19 16:49:13 +0900

    Update test_t2i.py with StableDiffusionPipeline and LCMScheduler

--- SD2Diffusers/test_t2i.py
+++ SD2Diffusers/test_t2i.py
@@ -1,20 +1,28 @@
-from diffusers import DiffusionPipeline
-import matplotlib.pyplot as plt
+from diffusers import StableDiffusionPipeline, LCMScheduler
+import torch
 
 DIFFUSERS_MODEL_PATH = "/Users/daiki/Documents/projects/sd-models/diffusers/Any45LCM"
 
-pipe = DiffusionPipeline.from_pretrained(DIFFUSERS_MODEL_PATH)
-pipe.enable_attention_slicing()
+device = "cpu"
+if torch.cuda.is_available():
+  device = "cuda"
+if torch.backends.mps.is_available():
+  device = "mps"
 
-# for apple silicon
-pipe = pipe.to("mps")
+pipe = StableDiffusionPipeline.from_pretrained(DIFFUSERS_MODEL_PATH,safety_checker=None)
+pipe.scheduler = LCMScheduler.from_config(pipe.scheduler.config)
+if device == "mps":
+  pipe.enable_attention_slicing()
+
+pipe = pipe.to(device)
 
 prompt = "1girl"
 output = pipe(
   prompt,
   num_inference_steps=6,
-  guidance_scale=1.0,
-  output_type="pil"
+  guidance_scale=1.5,
+  output_type="pil",
+  generator= torch.Generator(device).manual_seed(42),
   )
 
 image = output.images[0]
